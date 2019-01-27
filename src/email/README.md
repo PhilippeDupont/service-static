e03b168e30a1496dd7b503b1f739dce4318ea152
Benjamin Longearet <firehist@gmail.com>
2019-01-28 00:22:22 +0100

    Add new email generator

---
+++ src/email/README.md
@@ -0,0 +1,13 @@
+# Email signature
+
+All stuff related to email signature
+
+## How to update event information
+
+1. Replace the `./i_event.png` file to update the image at the end of the signature
+
+2. Update the redirect URL into `./index_event.html` file to redirect the user.
+
+## Signature templates
+
+All `./signature_*.html` files are template to copy/paste them into email software.
---
+++ src/email/generator.js
@@ -0,0 +1,146 @@
+console.log('Here we are 😆🚀')
+
+const removeAccents = (str) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+const signature = (person) => {
+  const {img, firstname, lastname, title, phone} = person
+  const firstnameSanitized = removeAccents(firstname)
+  const lastnameSanitized = removeAccents(lastname)
+  return `<table style="padding: 20px 0">
+    <tbody>
+      <tr>
+        <td style="padding: 0 30px;" valign="top">
+          <img width="90px" height="90px" src="./assets/${img}" />
+        </td>
+        <td>
+          <div style="font-family: Avenir-Heavy; font-size: 16px; color: #4A4A4A;letter-spacing: 0.82px; line-height: 22px;">
+            ${_.capitalize(firstname)} ${_.capitalize(lastname)}
+          </div>
+          <div style="font-family: Avenir-Book; font-size: 16px; color: #4A4A4A; letter-spacing: 0.82px; line-height: 22px; padding-bottom: 8px;">
+            ${title}
+          </div>
+
+          <div style="font-family: Avenir-Book; font-size: 16px; color: #9B9B9B; letter-spacing: 0.82px; line-height: 24px; padding-bottom: 12px;">
+            <div>
+                <img src="https://static.elium.tv/email/assets/i_mail.png" /> ${firstnameSanitized}.${lastnameSanitized}@elium.io
+            </div>
+            ${phone ? `<div>
+                <img src="https://static.elium.tv/email/assets/i_phone.png" /> ${phone}
+            </div>` : ''}
+          </div>
+
+          <div style="padding-bottom: 30px;">
+            <a href="https://twitter.com/Elium_tech" target="_blank" style="text-decoration: none">
+                <img src="https://static.elium.tv/email/assets/i_twitter.png" />
+            </a>
+            <a href="https://www.facebook.com/EliumTV" target="_blank" style="text-decoration: none">
+                <img src="https://static.elium.tv/email/assets/i_facebook.png" />
+            </a>
+            <a href="https://www.linkedin.com/company/elium" target="_blank" style="text-decoration: none">
+                <img src="https://static.elium.tv/email/assets/i_linkedin.png" />
+            </a>
+          </div>
+
+          <div>
+            <a href="https://www.elium.io" target="_blank" style="text-decoration: none">
+              <img width="80px" height="29px" src="https://static.elium.tv/email/assets/logo_elium.png" />
+            </a>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td style="padding-top: 30px">
+          <a href="https://static.elium.tv/email/index_event.html">
+            <img src="https://static.elium.tv/email/i_event.png">
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>`
+}
+
+const app = () => {
+  const people = [{
+    img: 'fabien_livet.png',
+    firstname: 'fabien',
+    lastname: 'livet',
+    title: 'CEO',
+    phone: '+33 (0) 6 66 90 97 45 '
+  }, {
+    img: 'benjamin_longearet.png',
+    firstname: 'benjamin',
+    lastname: 'longearet',
+    title: 'CTO',
+    phone: '+33 (0) 6 15 44 05 74 '
+  }, {
+    img: 'clementine_stora.png',
+    firstname: 'clémentine',
+    lastname: 'stora',
+    title: 'Artistic Director'
+  }]
+
+  const $inputHidden = $('#input-hidden')
+  const $people = $('#people')
+  const $signature = $('#signature')
+  const $signatureTabs = $('#signature .block__tab label')
+  const $tabContent = $('#tab-content')
+  const $btnCopy = $('#btn-copy')
+
+  let currentType = 'preview'
+  let currentPerson = people[0]
+  let currentSignature = signature(currentPerson)
+
+  $inputHidden.val(currentSignature)
+  updateTabContent()
+
+  // Build people list
+  $people.html(people.map(({img}, index) => `
+    <a class="pr-2${index === 0 ? ' active' : ''}" href="javascript:;" data-index="${index}">
+      <img src="assets/${img}">
+    </a>
+  `))
+
+  // Listen for click on a person
+  $people.on('click', 'a', event => {
+    const $currentElt = $(event.currentTarget)
+    if (!$currentElt.hasClass('active')) {
+      $people.find('a').removeClass('active')
+      $currentElt.addClass('active')
+      currentPerson = people[$currentElt.data('index')]
+      currentSignature = signature(currentPerson)
+      $inputHidden[0].value = currentSignature
+      updateTabContent()
+    }
+    event.stopPropagation()
+  })
+
+  // Listen for click on HTML/Preview
+  $signature.find('.block__tab').on('click', 'label', event => {
+    const $currentElt = $(event.currentTarget)
+    if(!$currentElt.hasClass('active')) {
+      $signatureTabs.removeClass('active')
+      $currentElt.addClass('active')
+      currentType = $(event.currentTarget).data('type')
+      updateTabContent()
+    }
+    event.stopPropagation()
+  })
+
+  // Handle copy button
+  $btnCopy.click(() => {
+    $inputHidden[0].select()
+    document.execCommand("copy");
+  })
+
+  // Update the tab content depending on the type
+  function updateTabContent() {
+    if (currentType === 'html') {
+      $tabContent.html(`<textarea style="width: 100%">${currentSignature}</textarea>`)
+    } else {
+      $tabContent.html(`${currentSignature}`)
+    }
+  }
+}
+
+// Boot the app
+app()
